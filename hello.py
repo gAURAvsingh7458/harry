6e82389eec49d03f44a048499e62b591550a98d0
gaurav singh <jnvgauravsingh@gmail.com>
2025-10-04 21:46:17 +0530

    added greet functon and more print statement

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
 print("Hello from Python on GitHub!")
-print("this is updated version")
+print("This is the updated version with more functionality.")
+
+# New code example
+def greet(name):
+    return f"Hello, {name}! Welcome to GitHub."
+
+print(greet("Gaurav"))
